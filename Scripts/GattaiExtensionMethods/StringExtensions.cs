ee7247d02efc3f7334e3a75aa8ca672f043cb03c
Gattai <108435021+BrunoGattai@users.noreply.github.com>
2023-04-15 18:51:19 -0300

    Update StringExtensions.cs

--- Scripts/GattaiExtensionMethods/StringExtensions.cs
+++ Scripts/GattaiExtensionMethods/StringExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace GattaiExtensionMethods
 {
@@ -12,7 +14,9 @@ namespace GattaiExtensionMethods
         public static void CopyToClipboard(this string source)
         {
             GUIUtility.systemCopyBuffer = source;
+#if UNITY_EDITOR
             EditorUtility.DisplayDialog("Copied to clipboard", $"Text: \n \n{source}", "OK");
+#endif
         }
 
         public static string Bold(this string source) => $"<b>{source}</b>";
